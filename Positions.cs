fcb8a6f80cb658aa34cc910b651ed8ea5e76c1c3
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-02 20:41:55 -0300

    Update Positions.cs

--- Positions.cs
+++ Positions.cs
@@ -1,23 +1,27 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace SnakeGame
 {
+    // Represents a position with row and column values
     public class Positions
     {
         public int Row { get; }
         public int Col { get; }
 
+        // Constructor to initialize the position with row and column values
         public Positions(int row, int col)
         {
             Row = row;
             Col = col;
         }
 
+        // Translates the position based on the given direction
         public Positions Translate(Directions dir)
         {
             return new Positions(Row + dir.RowOffset, Col + dir.ColOffset);
         }
 
+        // Override Equals method to compare two Positions objects
         public override bool Equals(object obj)
         {
             return obj is Positions positions &&
@@ -25,6 +29,7 @@ namespace SnakeGame
                    Col == positions.Col;
         }
 
+        // Override GetHashCode method to provide a unique hash code for the object
         public override int GetHashCode()
         {
             int hashCode = 1084646500;
@@ -33,11 +38,13 @@ namespace SnakeGame
             return hashCode;
         }
 
+        // Override the equality operator to compare two Positions objects
         public static bool operator ==(Positions left, Positions right)
         {
             return EqualityComparer<Positions>.Default.Equals(left, right);
         }
 
+        // Override the inequality operator to compare two Positions objects
         public static bool operator !=(Positions left, Positions right)
         {
             return !(left == right);
