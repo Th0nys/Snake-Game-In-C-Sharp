24e11e0bfa02180c0e6675d1637e4b0afdd6b327
Anthony Matheus Santos Gonçalves <86919567+Th0nys@users.noreply.github.com>
2023-08-02 20:43:36 -0300

    Update Directions.cs

--- Directions.cs
+++ Directions.cs
@@ -1,28 +1,34 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 
 namespace SnakeGame
 {
+    // Represents the possible directions that the snake can move
     public class Directions
     {
+        // Static fields representing the four cardinal directions
         public readonly static Directions Left = new Directions(0, -1);
         public readonly static Directions Right = new Directions(0, 1);
         public readonly static Directions Up = new Directions(-1, 0);
         public readonly static Directions Down = new Directions(1, 0);
 
+        // Row and Column offsets to represent the movement in each direction
         public int RowOffset { get; }
         public int ColOffset { get; }
 
+        // Private constructor to initialize the direction with row and column offsets
         private Directions(int rowOffset, int colOffset)
         {
             RowOffset = rowOffset;
             ColOffset = colOffset;
         }
 
+        // Returns the opposite direction to the current one
         public Directions Opposite()
         {
             return new Directions(-RowOffset, -ColOffset);
         }
 
+        // Override Equals method to compare two Directions objects
         public override bool Equals(object obj)
         {
             return obj is Directions directions &&
@@ -30,6 +36,7 @@ namespace SnakeGame
                    ColOffset == directions.ColOffset;
         }
 
+        // Override GetHashCode method to provide a unique hash code for the object
         public override int GetHashCode()
         {
             int hashCode = -1482510490;
@@ -38,11 +45,13 @@ namespace SnakeGame
             return hashCode;
         }
 
+        // Override the equality operator to compare two Directions objects
         public static bool operator ==(Directions left, Directions right)
         {
             return EqualityComparer<Directions>.Default.Equals(left, right);
         }
 
+        // Override the inequality operator to compare two Directions objects
         public static bool operator !=(Directions left, Directions right)
         {
             return !(left == right);
